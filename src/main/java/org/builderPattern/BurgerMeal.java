132ca846464d181f6df6a2f584e29a94a6a5cdf1
amanjoshi <ajoshi373@gmail.com>
2026-02-01 00:49:34 +0530

    Builder Design pattern

---
+++ src/main/java/org/builderPattern/BurgerMeal.java
@@ -0,0 +1,47 @@
+package org.builderPattern;
+
+public class BurgerMeal {
+
+    private final String  bun;
+    private final String patty;
+
+    private final boolean hasCheese;
+    private final String side;
+
+    private BurgerMeal(BurgerBuilder builder){
+        this.bun=builder.bun;
+        this.patty=builder.patty;
+        this.hasCheese=builder.hasCheese;
+        this.side=builder.side;
+    }
+
+    public static class BurgerBuilder {
+        private final String bun;
+        private final String patty;
+
+
+        private  boolean hasCheese;
+        private  String side;
+
+
+        public BurgerBuilder(String bun , String patty){
+            this.bun=bun;
+            this.patty=patty;
+        }
+
+        public BurgerBuilder withCheese(Boolean hasCheese){
+            this.hasCheese=hasCheese;
+            return this;
+        }
+
+        public BurgerBuilder withSide(String side){
+            this.side=side;
+            return this;
+        }
+
+        public BurgerMeal build(){
+            return new BurgerMeal(this);
+        }
+    }
+}
+
