19726186c8a61dbf2646d75a65a1cf41bdb87f47
amanjoshi <ajoshi373@gmail.com>
2026-02-02 23:46:56 +0530

    Singleton Design pattern

--- src/main/java/org/designPatterns/Singleton.java
+++ src/main/java/org/designPatterns/Singleton.java
@@ -10,7 +10,12 @@ public class Singleton {
     public static Singleton getInstance(){
 
         if(instance==null){
-            instance=new Singleton();
+            synchronized (Singleton.class){
+                if(instance==null){
+                    instance=new Singleton();
+                }
+            }
+
         }
         System.out.println("Singleton Instance is created");
         return instance;
