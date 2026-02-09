4f4fa76373bbf1d13b45c9d90480fcaaa6b90c25
amanjoshi <ajoshi373@gmail.com>
2026-02-09 23:56:15 +0530

    Composite Design pattern

--- src/main/java/behaviouralDesignPatterns/compositePattern/Client.java
+++ src/main/java/behaviouralDesignPatterns/compositePattern/Client.java
@@ -4,6 +4,7 @@ public class Client {
 
     public static void main (String[] args){
         //composite pattern
+
         File file1 = new File("Text.txt", 1);
         File file2 = new File("Pdf.pdf",2);
 
