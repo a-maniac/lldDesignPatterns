8958fd296133e1c6110dc05c8dcee51224f18e68
amanjoshi <ajoshi373@gmail.com>
2026-02-12 23:43:55 +0530

    Template Design pattern

--- src/main/java/behaviouralDesignPatterns/templateDesignPattern/BeverageClient.java
+++ src/main/java/behaviouralDesignPatterns/templateDesignPattern/BeverageClient.java
@@ -3,6 +3,7 @@ package behaviouralDesignPatterns.templateDesignPattern;
 public class BeverageClient {
 
     public static void main(String[] args){
+        //template design pattern
         Beverage tea = new Tea();
         tea.prepareBeverage();
 
